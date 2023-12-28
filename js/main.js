3f51267780ba0c0095245f44a0fef72240bd7662
CarlosSimon02 <caloycamacho02@gmail.com>
2023-12-28 11:28:43 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,10 @@
-import Controller from './controller.js';
-// import {$on} from './helpers';
-import Template from './template.js';
-import Store from './store.js';
-import View from './view.js';
+import TimerController from "./controllers/TimerController.js";
+import TimerStore from "./stores/TimerStore.js";
+import TimerView from "./views/TimerView.js";
 
-const store = new Store();
-
-const template = new Template();
-const view = new View(template);
-
-/**
- * @type {Controller}
- */
-const controller = new Controller(store, view);
+const store = new TimerStore();
+const view = new TimerView();
+const controller = new TimerController(store, view);
 
 // const setView = () => controller.setView(document.location.hash);
 // $on(window, 'load', setView);
