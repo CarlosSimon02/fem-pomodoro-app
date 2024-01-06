d1883a030838c25d27e0d2f0a06497ca01a7ee79
CarlosSimon02 <caloycamacho02@gmail.com>
2024-01-06 10:20:24 +0800

    Add service workers

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js"
+);
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|webp|svg|ico)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: "images",
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "static-resources",
+  })
+);
+
+registerRoute(
+  ({ url }) => url.origin === "https://fonts.googleapis.com",
+  new StaleWhileRevalidate({
+    cacheName: "google-fonts-stylesheets",
+  })
+);
+
+registerRoute(
+  ({ url }) => url.origin === "https://fonts.gstatic.com",
+  new StaleWhileRevalidate({
+    cacheName: "google-fonts-webfonts",
+  })
+);
